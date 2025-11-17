a2ad33436d7a8f887e5130ccad091016be1cbddf
jm.son <jm.son@weeds.co.kr>
2025-11-17 17:30:45 +0900

    git action test

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+// 테스트용
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
